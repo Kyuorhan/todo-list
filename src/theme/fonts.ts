6cc3b78a9acdde3a928c727179bbf2253e2ec2f0
Jhonny Kyuorhan <jkdevprogrammer@gmail.com>
2024-04-18 19:34:02 -0400

    Project initial

---
+++ src/theme/fonts.ts
@@ -0,0 +1,17 @@
+export const fonts = {
+  family: {
+    regular: "Inter_400Regular",
+    medium: "Inter_500Medium",
+    semibold: "Inter_600SemiBold",
+    bold: "Inter_700Bold",
+    // regular: "Poppins_400Regular",
+    // medium: "Poppins_500Medium",
+    // semibold: "Poppins_600SemiBold",
+    // bold: "Poppins_700Bold",
+  },
+  size: {
+		sm: 12,
+		md: 14,
+		lg: 16,
+  },
+}
